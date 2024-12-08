696d668887bdac61f4d0a73f926dfc514e12c2b0
Christian Kuhre <christian.kuhre@gmail.com>
2024-12-08 19:50:05 +0300

    Implement Day 5

---
+++ src/day_five/mod.rs
@@ -0,0 +1,37 @@
+use std::{cmp::Ordering, collections::HashSet, fs};
+
+pub fn implementation() {
+    let input = fs::read_to_string("src/day_five/input").expect("Should read full input file");
+
+    let mut input_split = input.split("\n\n");
+
+    let mut order_set = HashSet::<&str>::new();
+
+    for line in input_split.nth(0).unwrap().lines() {
+        order_set.insert(line);
+    }
+
+    let mut one_star = 0;
+    let mut two_star = 0;
+
+    'update_loop: for line in input_split.nth(0).unwrap().lines() {
+        let mut update: Vec<&str> = line.split(",").collect();
+
+        for (i, instruction) in update.iter().enumerate() {
+            let is_not_ordered = update[i..].iter().any(|value| order_set.contains(&format!("{}|{}", value, instruction).as_str()));
+
+            if is_not_ordered {
+                update.sort_by(|a, b| if order_set.contains(&format!("{}|{}", a, b).as_str()) { Ordering::Less } else { Ordering::Greater });
+
+                two_star += update.get(update.len() / 2).unwrap().parse::<i32>().unwrap();
+
+                continue 'update_loop;
+            }
+        }
+
+        one_star += update.get(update.len() / 2).unwrap().parse::<i32>().unwrap();
+    }
+
+    println!("One Star: {one_star:?}");
+    println!("Two Star: {two_star:?}");
+}
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ mod day_one;
 mod day_two;
 mod day_three;
 mod day_four;
+mod day_five;
 
 fn main() {
     //day_one::implementation();
     //day_two::implementation();
     //day_three::implementation();
-    day_four::implementation();
+    //day_four::implementation();
+    day_five::implementation();
 }
